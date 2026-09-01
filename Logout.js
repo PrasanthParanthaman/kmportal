238d67942ef5f08c3231b405f9823517498aa8f5
PrasanthParanthaman <Prasanth.P20fb0e@cognizant.com>
2026-09-01 15:23:41 +0530

    Add files via upload

---
+++ Logout.js
@@ -0,0 +1,4 @@
+// This script waits for 4 seconds and then redirects to the specified page.
+setTimeout(function() {
+    window.location.href = "/"; // Replace with the URL of the page you want to redirect to.
+}, 2000); // 3000 milliseconds = 3 seconds
